945e42e0e43c23a1b4b8498933cb44ce2bcf4a8d
unknown <97494h@PWF00685.maif.local>
2015-03-23 17:27:22 +0100

    ajout connection a mongo

--- app/controllers/api/users.go
+++ app/controllers/api/users.go
@@ -5,13 +5,75 @@ import (
  		"gotest1/app/models"
  		"strconv"
  		"fmt"
+ 		"log"
+        "gopkg.in/mgo.v2"
+        "gopkg.in/mgo.v2/bson"
 )
 
 type APIUsers struct {
 	*revel.Controller
 }
 
-var users []models.User = []models.User{{"0", "John Doo"}, {"1", "Maria Luis"}}
+var users []models.User = []models.User{{"0", "John Doo", "0","0","0","0","0","0"}, {"1", "Maria Luis","0","0","0","0","0","0"}}
+
+func (c APIUsers) Login() revel.Result {
+    session, err := mgo.Dial("localhost")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+
+    // Optional. Switch the session to a monotonic behavior.
+    session.SetMode(mgo.Monotonic, true)
+
+    c1 := session.DB("test").C("users")
+    /*err = c1.Insert(&user1, &user2, &user3)
+    if err != nil {
+            log.Fatal(err)
+    }
+	*/
+
+	result := models.User{}
+    err = c1.Find(bson.M{"username": "jdoo", "password" : "password"}).One(&result)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("User:", result)
+
+	return c.RenderJson(result)
+}
+
+func (c APIUsers) CreateUsers() revel.Result {
+	fmt.Println("CreateUsers()")
+	user1 := models.User{"1", "jdoo","John","Doo","john@doo","0","0","password"}
+	user2 := models.User{"2", "mluis","Maria","Luis","maria@luis","0","0","password"}
+	user3 := models.User{"3", "test1","firstn","lastn","test1@test1","0","0","password"}
+
+	var users [3]models.User
+	users[0] = user1
+	users[1] = user2
+	users[2] = user3
+
+    session, err := mgo.Dial("localhost")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+
+    // Optional. Switch the session to a monotonic behavior.
+    session.SetMode(mgo.Monotonic, true)
+
+    c1 := session.DB("test").C("users")
+    err = c1.Insert(&user1, &user2, &user3)
+    if err != nil {
+            log.Fatal(err)
+    }
+
+
+	return c.RenderJson(users)
+}
+
 
 func (c APIUsers) Me() revel.Result {
 	fmt.Println("Me()")
@@ -22,7 +84,7 @@ func (c APIUsers) Me() revel.Result {
 func (c APIUsers) List() revel.Result {
 	fmt.Println("List()")
 
-	newUser := models.User{string(len(users)), "user" + string(strconv.Itoa(len(users)))}
+	newUser := models.User{string(len(users)), "user" + string(strconv.Itoa(len(users))),"0","0","0","0","0","0"}
 	users = append(users, newUser)	
 
 	//users := c.Init()
--- app/models/user.go
+++ app/models/user.go
@@ -4,4 +4,10 @@ package models
 type User struct {
   Id string 
   Username string
+  Firstname string
+  Lastname string
+  Email string
+  TwitterUid string
+  FacebookUid string
+  Password string `json:"password" bson:"password"`
 }
